9c32444ec7a97037bae0bac0aa4a694ce95fb92c
Cenk Arioz <cenkarioz@gmail.com>
2022-06-30 20:16:55 +0300

    create tx channel

--- kafkautils/producer.go
+++ kafkautils/producer.go
@@ -21,6 +21,7 @@ type ProducerInterface interface {
 	CommitTransaction(context.Context) error
 	AbortTransaction(context.Context) error
 	EnableTransactions() error
+	ProduceMsg(Topic, proto.Message, chan kafka.Event) error
 	WriteAndCommitSink(<-chan *Message)
 	WriteAndCommit(Topic, []byte, proto.Message) error
 	MakeQueueTransactionSink() chan *Message
@@ -334,4 +335,25 @@ func (p *Producer) MakeQueueTransactionSink() chan *Message {
 	go p.WriteAndCommitSink(sink)
 
 	return sink
+}
+
+// ProduceMsg sends single message to Kafka
+func (p *Producer) ProduceMsg(topic Topic, msg proto.Message, delivery chan kafka.Event) error {
+	if p.closed {
+		return fmt.Errorf("cannot produce message, producer is already closed")
+	}
+
+	pbData, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	monitor.SetMetricsForKafkaLastWriteTime()
+
+	topicString := topic.String()
+
+	return p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topicString, Partition: kafka.PartitionAny},
+		Value:          pbData,
+	}, delivery)
 }
